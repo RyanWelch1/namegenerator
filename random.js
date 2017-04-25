6ebdf95edac186582cb849cf6daf2d63e78ea350
RyanWelch1 <ryan.welch1@live.com>
2017-04-24 20:33:02 -0500

    first commit

---
+++ random.js
@@ -0,0 +1,11 @@
+var ryansFirstName = "Ryan";
+var ryansLastName = "Welch";
+var jaredsFirstName = "Jared";
+var jaredsLastName = "Hensley";
+
+
+var nameGenerator = function(firstName, lastName) {
+	console.log(firstName + " " + lastName);
+};
+  nameGenerator(ryansFirstName, ryansLastName);
+  nameGenerator(jaredsFirstName, jaredsLastName);
